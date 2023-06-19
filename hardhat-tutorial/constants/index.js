d254806e688a6a82a7560b8226fda50251d1cfd2
Gayathri Krishnan <gayathri.krishnan.designer@gmail.com>
2023-06-19 22:21:36 +0530

    Initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x2F586c5417482c8E44f311221B8fe0881b699027";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
